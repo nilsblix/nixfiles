c87149592f50c1bcd3cbb0e77b14729e024a2698
nilsblix <nilsblix06@gmail.com>
2026-06-06 00:46:11 +0200

    half working fish as default shell

--- modules/feats/packages.nix
+++ modules/feats/packages.nix
@@ -18,6 +18,7 @@
             dust
             fd
             firefox
+            fish
             gh
             git
             postman
--- modules/feats/shell.nix
+++ modules/feats/shell.nix
@@ -1,47 +1,38 @@
-{ inputs, ... }:
+{ ... }:
 {
     flake.modules.darwin.shell = {
         programs.zsh.enable = true;
     };
 
-    flake.modules.homeManager.shell = { lib, pkgs, ... }:
-    let
-        shellAliases = shell: {
-            c = "cd $(fd --type=dir | fzf)";
-            codex = "nix run github:numtide/llm-agents.nix#codex";
-            da = "direnv allow";
-            drs = "sudo darwin-rebuild build switch --flake";
-            gd = "git diff";
-            gs = "git status";
-            ll = "ls -l";
-            ls = "ls --color=tty";
-            nd = "nix develop -c " + shell;
-            nrs = "sudo nixos-rebuild switch --flake";
-            nvim = "~/neovim-flake/result/bin/nvim";
-        };
-
-        glowstick =
-            inputs.nilsblix-glowstick.packages.${pkgs.stdenv.hostPlatform.system}.default;
-
-        yaziCdScript = ''
-            function y() {
-                local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-                yazi "$@" --cwd-file="$tmp"
-                IFS= read -r -d "" cwd < "$tmp"
-                [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-                rm -f -- "$tmp"
-            }
-        '';
-    in {
+    flake.modules.homeManager.shell = { ... }: {
         programs.zsh = {
             enable = true;
-            shellAliases = shellAliases "zsh";
-            initContent = lib.concatStrings [
-                ''
-                    eval "$(${glowstick}/bin/main zsh default-nix)"
-                ''
-                yaziCdScript
-            ];
+            initContent = ''
+                fish
+            '';
+        };
+
+        programs.fish = {
+            enable = true;
+            shellInit = ''
+                set -U fish_greeting
+
+                function y
+                    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+                    command yazi $argv --cwd-file="$tmp"
+                    if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+                        builtin cd -- "$cwd"
+                    end
+                    command rm -f -- "$tmp"
+                end
+
+                bind \cc 'echo; commandline ""; commandline -f repaint'
+
+                alias gd "git diff"
+                alias gs "git status"
+                alias nd "nix develop -c fish"
+                alias nvim "~/neovim-flake/result/bin/nvim";
+            '';
         };
     };
 
